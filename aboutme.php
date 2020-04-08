c64fbcaf0c6f82476b01c4d8a9f0b1101115614f
anisazahrawidyanti <55775447+anisazahrawidyanti@users.noreply.github.com>
2020-04-08 13:23:41 +0700

    menambahkan no hp dan email serta menghapus alamat

--- aboutme.php
+++ aboutme.php
@@ -2,5 +2,6 @@
     echo "Nama  : Anisa Zahra Widyanti <br>";
     echo "Kelas : XI RPL 2 <br>";
     echo "Asal Sekolah : SMK Taruna Bhakti Depok <br>";
-    echo "Alamat Rumah : Gg.Nangka RT.01 RW.02 Sukamaju Baru Tapos Depok";
+    echo "No Handphone : 089644192836";
+    echo "Email : anisazahrawidyanti123@gmail";
 ?>
