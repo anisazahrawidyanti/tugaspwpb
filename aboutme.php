5f956d4c225ddd38939600f520f524ba353e7709
anisazahrawidyanti <55775447+anisazahrawidyanti@users.noreply.github.com>
2020-04-08 13:09:49 +0700

    aboutme.php

---
+++ aboutme.php
@@ -0,0 +1,6 @@
+<?php
+    echo "Nama  : Anisa Zahra Widyanti <br>";
+    echo "Kelas : XI RPL 2 <br>";
+    echo "Asal Sekolah : SMK Taruna Bhakti Depok <br>";
+    echo "Alamat Rumah : Gg.Nangka RT.01 RW.02 Sukamaju Baru Tapos Depok";
+?>
